d9a8b47a2df5cbcdbf09ee0b2bfd317cc943322b
Technology Department <technology@JThobaben-4.local>
2016-01-26 11:00:53 -0500

    Make a basic User model (including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/new'
+
   root 'static_pages#home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+ 
+  get "help" => 'static_pages#help'
+ 
+  get "about" => 'static_pages#about'
+ 
+  get "login" => 'static_pages#login'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
